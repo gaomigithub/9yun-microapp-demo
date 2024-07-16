eb4977011c5f4c870a10f71afe962021a56b7263
Mi Gao <398938578@qq.com>
2024-07-16 19:40:37 +0800

    测试umi子项目接入 part1

--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -1,11 +1,53 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import install from './install'
-import './permission'
-import './styles/index.scss'
-import 'virtual:svg-icons-register'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./public-path";
+import install from "./install";
+import "./permission";
+import "./styles/index.scss";
+import "virtual:svg-icons-register";
+import { registerMicroApps, start } from "qiankun";
 
-const app = createApp(App)
-console.log(app)
-app.use(install)
-app.mount('#app')
+registerMicroApps([
+  //   {
+  //     // 微应用的名称，后续微应用通过此名称进行识别
+  //     name: "sub-app-1",
+  //     // 微应用的入口
+  //     entry: "http://localhost:5001/",
+  //     // 微应用加载的容器，与上面的id需保持完全一致
+  //     container: "#sub-app-1-container",
+  //     // 激活微应用的规则（路由），需要与路由文件的路径保持一致，同时需要与子应用嵌入页面的路由保持一致
+  //     activeRule: "/sub-app-1",
+  //     // 向子应用传递参数
+  //     props: {
+  //       data: "Hello App1 -from mainApp",
+  //     },
+  //   },
+  {
+    name: "sub-app-2",
+    entry: "//localhost:5002/sub-app-2/",
+    container: "#sub-app-2-container",
+    activeRule: "/microapp/sub-app-2",
+    props: {
+      data: "Hello App2 -from mainApp",
+    },
+  },
+]);
+
+start();
+//     {
+//   sandbox: {
+//     // strictStyleIsolation: true, // 开启严格的样式隔离模式
+//     experimentalStyleIsolation: true, // 开启后所有样式都会加上一个类名 .app-main {}
+//     //    ===>  div[data-qiankun-react16] .app-main {}
+//   },
+//   singular: false, // 单一时间只渲染一个微应用，默认为true
+// }
+
+// 独立运行时
+// @ts-ignore
+if (!window.__POWERED_BY_QIANKUN__) {
+  const app = createApp(App);
+  console.log(app);
+  app.use(install);
+  app.mount("#app");
+}
---
+++ packages/main/src/views/microapp/sub-app-2/index.vue
@@ -0,0 +1,86 @@
+<template>
+    <div id="sub-app-2-container" class="integrate-page"></div>
+</template>
+
+<script>
+export default {
+    name: 'SubAppTwoContainer'
+}
+</script>
+
+<style lang="less">
+.integrate-page {
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    border: 0;
+    background: linear-gradient(180deg, #bcbfc7 0%, #c6cddc 100%);
+}
+</style>
+
+
+<!-- 
+
+<template>
+    <div id="container-sub-app"></div>
+</template>
+
+<script>
+import { loadMicroApp } from 'qiankun';
+// import actions from '../../actions.js';
+
+export default {
+    name: 'microApp',
+    // mixins: [actions],
+    data() {
+        return {
+            microApp: null,
+        };
+    },
+    mounted() {
+        const getMicroInfo = this.getMicroInfo();
+        this.microApp = loadMicroApp(getMicroInfo, { singular: true });
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy...');
+        this.microApp.unmount();
+    },
+    methods: {
+        // 手动加载微应用
+        getMicroInfo() {
+            const appIdentifying = this.$route.path.split('/')[1];
+            let data = {};
+            const href = window.location.host;
+            console.log('href::', href)
+            for (let i = 0; i < document.subApps.length; i++) {
+                const element = document.subApps[i];
+                if (element.activeRule.includes(appIdentifying)) {
+                    if (typeof element.entry !== 'string') {
+                        data = {
+                            ...element,
+                            entry: element.entry[href]
+                                ? element.entry[href]
+                                : Object.values(element.entry)[0],
+                        };
+                    } else {
+                        data = { ...element };
+                    }
+                    data.props = {
+                        token: {
+                            userInfo: {
+                                userName: '小明',
+                                userId: '123',
+                                date: new Date().toLocaleString(),
+                            },
+                        },
+                    };
+                    data.activeRule = [appIdentifying];
+                    break;
+                }
+            }
+            console.log('data::', data);
+            return data;
+        },
+    },
+};
+</script> -->
